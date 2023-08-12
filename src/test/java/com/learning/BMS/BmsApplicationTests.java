11224a346ba1c3784faeb5fb65707cdf8ca435a1
imakashgusain <akashgusain57@gmail.com>
2023-08-12 11:58:00 +0530

    inital commit

---
+++ src/test/java/com/learning/BMS/BmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.BMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
